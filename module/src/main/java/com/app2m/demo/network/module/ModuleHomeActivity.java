a737255022d8919befbbcfaf0759c453f61ff57a
conghao <hao.cong@qq.com>
2017-03-12 23:11:18 +0800

    init the first module of android library

--- module/src/main/java/com/app2m/demo/network/module/ModuleHomeActivity.java
+++ module/src/main/java/com/app2m/demo/network/module/ModuleHomeActivity.java
@@ -1,13 +1,17 @@
 package com.app2m.demo.network.module;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.app2m.demo.network.module.databinding.ActivityModuleHomeBinding;
+
 public class ModuleHomeActivity extends BaseActivity {
+    private ActivityModuleHomeBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_module_home);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_module_home);
     }
 }
---
+++ module/src/main/java/com/app2m/demo/network/module/MyConstants.java
@@ -0,0 +1,13 @@
+package com.app2m.demo.network.module;
+
+/**
+ * Created by CongHao on 2017/3/12.
+ * E-mail: hao.cong@app2m.com
+ */
+
+public class MyConstants {
+    private MyConstants() {
+        throw new IllegalAccessError("can not access the method.");
+    }
+    public static final String GJJ_HOST = "";
+}
