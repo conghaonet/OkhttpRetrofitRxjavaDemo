d4672be9597ea07427290f6a1700db9b2091c04c
conghao <hao.cong@qq.com>
2017-03-12 23:06:13 +0800

    init the first module of android library

--- app/src/main/java/com/app2m/demo/network/MainActivity.java
+++ app/src/main/java/com/app2m/demo/network/MainActivity.java
@@ -1,7 +1,11 @@
 package com.app2m.demo.network;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.app2m.demo.network.module.ModuleHomeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openModuleHomeActivity(View view) {
+        startActivity(new Intent(this, ModuleHomeActivity.class));
     }
 }
---
+++ module/src/main/java/com/app2m/demo/network/module/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.app2m.demo.network.module;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
